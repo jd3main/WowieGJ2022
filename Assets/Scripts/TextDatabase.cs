38a2157912630048a2f182ebc325e9e736bfd37f
Jamie <jd3main@gmail.com>
2022-08-13 21:07:03 +0800

    Read text data from tsv
    * Reimport Odin due to some errors

--- Assets/Scripts/TextDatabase.cs
+++ Assets/Scripts/TextDatabase.cs
@@ -12,149 +12,120 @@ using System.IO;
 using Sirenix.OdinInspector.Editor;
 #endif
 
-namespace _EI
+[ShowOdinSerializedPropertiesInInspector]
+[ExecuteAlways]
+public class TextDatabase : MonoBehaviour
 {
-    [ShowOdinSerializedPropertiesInInspector]
-    public class TextDatabase : MonoBehaviour
+    static readonly string[] ENDL = { "\r\n", "\r", "\n" };
+    [ShowInInspector]
+    public static TextDatabase instance;
+
+    public char delim = '\t';
+    public string filename = "dialogue";
+    public int idColumnIndex = 2;
+    public int valueColumnIndex = 4;
+
+    [ShowInInspector]
+    public static Dictionary<string, TextData> textDataDict = new Dictionary<string, TextData>();
+
+
+    private void OnEnable()
     {
-        static readonly string[] ENDL = { "\r\n", "\r", "\n" };
+        Init();
+    }
 
-        public static string filename = "TextData/zh-tw";
+    private void Init()
+    {
+        instance = this;
+        Load();
+    }
 
-        [ShowInInspector, OnCollectionChanged(after: "OnTextDataDictChanged")]
-        public static Dictionary<string, TextData> textDataDict = new Dictionary<string, TextData>();
 
+    public string GetValue(string id)
+    {
+        if (string.IsNullOrEmpty(id) || !textDataDict.ContainsKey(id))
+            return null;
+        return textDataDict[id].value;
+    }
 
-        public static string GetValue(string id)
+    [ShowInInspector]
+    public void Load()
+    {
+        Debug.Log($"Load text data from {filename}");
+
+        TextAsset csv = Resources.Load<TextAsset>(filename);
+
+        if (csv == null)
+            throw new Exception($"{filename} not found");
+
+        textDataDict = new Dictionary<string, TextData>();
+
+        string[] rows = csv.text.Split(ENDL, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string row in rows)
         {
-            if (string.IsNullOrEmpty(id) || !textDataDict.ContainsKey(id))
-                return null;
-            return textDataDict[id].value;
-        }
-
-        [ShowInInspector]
-        public static void Load()
-        {
-            Debug.Log($"Load text data from {filename}");
-
-            TextAsset csv = Resources.Load<TextAsset>(filename);
-
-            if (csv == null)
-                throw new Exception($"{filename} not found");
-
-            textDataDict = new Dictionary<string, TextData>();
-
-            string[] rows = csv.text.Split(ENDL, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string row in rows)
+            if (string.IsNullOrEmpty(row))
+                continue;
+            string[] values = row.Split(delim);
+            if (idColumnIndex < values.Length && valueColumnIndex < values.Length)
             {
-                if (string.IsNullOrEmpty(row))
-                    continue;
-                TextData textData = new TextData(row.Split(','));
+                string id = values[idColumnIndex];
+                string value = values[valueColumnIndex];
+                TextData textData = new TextData(id, value);
                 textDataDict[textData.id] = textData;
             }
         }
+    }
 
 #if UNITY_EDITOR
-        public static void SetValue(string id, string value)
-        {
-            if (textDataDict.ContainsKey(id))
-                textDataDict[id].value = value;
-            else
-                textDataDict[id] = new TextData(id, value);
-            Save();
-        }
-
-        [ShowInInspector]
-        public static void Save()
-        {
-            TextAsset textAsset = Resources.Load<TextAsset>(filename);
-            string path = AssetDatabase.GetAssetPath(textAsset);
-
-            Debug.Log($"Save text data to {path}");
-
-            if (textDataDict == null)
-                textDataDict = new Dictionary<string, TextData>();
-
-            using (StreamWriter sw = File.CreateText(path))
-            {
-                foreach (TextData textData in textDataDict.Values)
-                {
-                    string line = $"{textData.id},{textData.value}";
-                    sw.WriteLine(line);
-                    Debug.Log(line);
-                }
-            }
-        }
-
-        public void OnTextDataDictChanged(CollectionChangeInfo info, object value)
-        {
-            Save();
-        }
-
-        [UnityEditor.Callbacks.DidReloadScripts]
-        public static void OnScriptsReloaded()
-        {
-            Init();
-        }
+    [UnityEditor.Callbacks.DidReloadScripts]
+    public void OnScriptsReloaded()
+    {
+        Init();
+    }
 #endif  // UNITY_EDITOR
 
-        private static void Init()
-        {
-            Load();
-        }
+}
 
-        private void Awake()
-        {
-            Init();
-        }
+
+[Serializable]
+public class TextData
+{
+    public string id;
+    public string value;
+
+
+    public TextData(string id, string value)
+    {
+        this.id = id;
+        this.value = value;
+    }
+}
+
+
+[Serializable]
+public class RefText
+{
+    [ValueDropdown("GetTextDataPreview")]
+    [SerializeField]
+    private string _id;
+    public string id => _id;
+
+    [ShowInInspector]
+    public string Value
+    {
+        get => TextDatabase.instance.GetValue(id);
+        //set => TextDatabase.instance.SetValue(id, value);
     }
 
-
-    [Serializable]
-    public class TextData
+    public static implicit operator string(RefText refText)
     {
-        public string id;
-        public string value;
-
-        public TextData(string[] values)
-        {
-            id = values[0];
-            value = values[1];
-        }
-
-        public TextData(string id, string value)
-        {
-            this.id = id;
-            this.value = value;
-        }
+        return refText.Value;
     }
 
-
-    [Serializable]
-    public class RefText
+    public static IEnumerable<ValueDropdownItem<string>> GetTextDataPreview()
     {
-        [ValueDropdown("GetTextDataPreview")]
-        [SerializeField]
-        private string _id;
-        public string id => _id;
-
-        [ShowInInspector]
-        public string Value
-        {
-            get => TextDatabase.GetValue(id);
-            set => TextDatabase.SetValue(id, value);
-        }
-
-        public static implicit operator string(RefText refText)
-        {
-            return refText.Value;
-        }
-
-        public static IEnumerable<ValueDropdownItem<string>> GetTextDataPreview()
-        {
-            return TextDatabase.textDataDict.Values.Select(
-                data => new ValueDropdownItem<string>($"{data.id} | {data.value}", data.id)).ToArray();
-        }
+        return TextDatabase.textDataDict.Values.Select(
+            data => new ValueDropdownItem<string>($"{data.id} | {data.value}", data.id)).ToArray();
     }
 }
