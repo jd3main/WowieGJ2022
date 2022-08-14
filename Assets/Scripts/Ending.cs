893209ab4d50a4ed44a93ccbe6b57ea6780fbb73
Jamie <jd3main@gmail.com>
2022-08-14 17:46:43 +0800

    Implement all endings

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    public DialogueSystem dialogueSystem;
+    public List<RefText> endingInfos;
+
+    private void Start()
+    {
+        dialogueSystem = DialogueSystem.instance;
+        dialogueSystem.onDialogueClosed.AddListener(OnCloseDialogue);
+    }
+
+    public void OnCloseDialogue()
+    {
+        StartCoroutine(_OnCloseDialogue());
+    }
+
+    public IEnumerator _OnCloseDialogue()
+    {
+        yield return null;
+
+        EndingManager endingManager = FindObjectOfType<EndingManager>();
+        endingManager.dialoguePanel.SetActive(false);
+        dialogueSystem.textUI = endingManager.endingInfoTextUI;
+        dialogueSystem.textUI.gameObject.SetActive(true);
+
+        yield return null;
+
+        foreach (var s in endingInfos)
+            dialogueSystem._Enqueue(s);
+
+        dialogueSystem.onDialogueClosed.RemoveListener(OnCloseDialogue);
+        dialogueSystem.onDialogueClosed.AddListener(endingManager.Restart);
+    }
+
+}
--- Assets/Scripts/EndingManager.cs
+++ Assets/Scripts/EndingManager.cs
@@ -1,10 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using TMPro;
 
 public class EndingManager : MonoBehaviour
 {
-    public List<RefText> dialogues;
+    public RelationStatus relationShip;
+    public ShipStatus spaceShip;
+    public TextMeshPro endingInfoTextUI;
+    public GameObject dialoguePanel;
+    public SceneTransition transition;
+
+    private void Start()
+    {
+        string name = $"Ending_{relationShip.ToString()[0]}_{spaceShip.ToString()[0]}";
+        var endings = FindObjectsOfType<Ending>(true);
+        var targetEnding = endings.Where(e => e.gameObject.name == name).First();
+        GameObject endingGO = targetEnding.gameObject;
+        endingGO.SetActive(true);
+    }
 
 
+    public void Restart()
+    {
+        StartCoroutine(_Restart());
+    }
+
+    public IEnumerator _Restart()
+    {
+        transition.duration = 2.0f;
+        transition.Play();
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("SpaceShip");
+    }
+}
+
+public enum RelationStatus
+{
+    Normal,
+    Crazy,
+    Friend,
+    Seeing,
+}
+
+public enum ShipStatus
+{
+    Arrive,
+    Crash,
+    Drift,
+    Boom,
 }
